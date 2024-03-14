ff77ce7b8f1e49ba90371184faae147aa0c16ece
Joel Joe <joelezra10@gmail.com>
2024-03-14 18:45:31 +0000

    add home app as landing page and connect to project

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("warathai")
