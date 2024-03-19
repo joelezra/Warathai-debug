4c14226b34ac07b0093c076aeec589650232431a
Joel Joe <joelezra10@gmail.com>
2024-03-19 17:59:18 +0000

    add booking model

---
+++ booking/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+# Create your models here.
+class TimeSlot(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+
+class Date(models.Model):
+    date = models.DateField()
+
+
+class Table(models.Model):
+    capacity = models.IntegerField()
+
+
+class Booking(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.ForeignKey(Date, on_delete=models.CASCADE)
+    time_slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+    num_people = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        # Ensure the table has sufficient capacity for the booking
+        if num_people > self.table.capacity:
+            raise ValidationError("The table's capacity is insufficient for this booking.")
+
+        # Ensure the selected time slot and date are valid
+        existing_bookings = Booking.objects.filter(date=self.date, time_slot=self.time_slot)
+        total_people = sum(booking.num_people for booking in existing_bookings)
+        if total_people + self.num_people > self.table.capacity:
+            raise ValidationError("The selected time slot is unavailable.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean() # Perform full validation before saving
+        super().save(*args, **kwargs)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    item_image = CloudinaryField('image', default='placeholder')
+    allergens = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
